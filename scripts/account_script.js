c89caabaecbfecbe0f2d5ccc4122920853164467
Justas Gužė <gjustas000@gmail.com>
2023-07-21 21:44:59 +0300

    Fixed input error display

--- scripts/account_script.js
+++ scripts/account_script.js
@@ -16,59 +16,48 @@ if (document.getElementById('signup') !== null){
     confirm_password_label = document.getElementById('confirm_password')
 
     if (current_error == 'user-taken'){
-        username_inp.className = 'failed'
-        username_label.className = 'user-taken'
+        username_inp.classList.add('failed')
+        username_label.classList.add('email-taken')
     }
     if (current_error == 'email-taken'){
-        email_inp.className = 'failed'
-        email_label.className = 'email-taken'
+        email_inp.classList.add('failed')
+        email_label.classList.add('email-taken')
     }
 
     form.addEventListener("submit", (event) => {
-
-        username_inp.className = ''
-        email_inp.className = ''
-        password_inp.className = ''
-        confirm_password_inp.className = ''
-
-        username_label.className = ''
-        email_label.className = ''
-        password_label.className = ''
-        confirm_password_label.className = ''
-
         let error = false
         let valid_email = /\S+@\S+\.\S+/
 
         if (password_inp.value !== confirm_password_inp.value){
-            password_inp.className = 'failed'
-            password_label.className = 'passwords-no-match'
-            confirm_password_inp.className = 'failed'
-            confirm_password_label.className = 'passwords-no-match'
+            password_inp.classList.add('failed')
+            password_label.classList.add('passwords-no-match')
+            confirm_password_inp.classList.add('failed')
+            confirm_password_label.classList.add('passwords-no-match')
             error = true
         }
         if (valid_email.test(email_inp.value) === false){
-            email_inp.className = 'failed'
-            email_label.className = 'invalid-email'
+            email_inp.classList.add('failed')
+            email_label.classList.add('invalid-email')
             error = true
         }
         if (username_inp.value === "" || username_inp == null){
-            username_inp.className = 'failed'
-            username_label.className = 'empty-field'
+            username_inp.classList.add('failed')
+            username_label.classList.add('empty-field')
             error = true
         }
         if (email_inp.value === "" || email_inp == null){
-            email_inp.className = 'failed'
-            email_label.className = 'empty-field'
+            email_inp.classList.add('failed')
+            email_label.classList.add('empty-field')
             error = true
         }
         if (password_inp.value === "" || password_inp == null){
-            password_inp.className = 'failed'
-            password_label.className = 'empty-field'
+            password_inp.classList.add('failed')
+            password_label.classList.add('empty-field')
             error = true
         }
         if (confirm_password_inp.value === "" || confirm_password_inp == null){
-            confirm_password_inp.className = 'failed'
-            confirm_password_label.className = 'empty-field'
+            confirm_password_inp.classList.add('failed')
+            confirm_password_label.classList.add('empty-field')
             error = true
         }
         if (error === true){
@@ -89,30 +78,25 @@ if (document.getElementById('login') !== null){
     const url = new URL(window.location.toLocaleString())
     let current_error = url.searchParams.get('error')
     if (current_error == 'wrong-password'){
-        password_inp.className = 'failed'
-        password_label.className = 'wrong-password'
+        password_inp.classList.add('failed')
+        password_label.classList.add('wrong-password')
     }
     if (current_error == 'wrong-email'){
-        email_inp.className = 'failed'
-        email_label.className = 'wrong-email'
+        email_inp.classList.add('failed')
+        email_label.classList.add('wrong-email')
     }
 
     form.addEventListener("submit", (event) => {
         let error = false
-        email_inp.className = ''
-        password_inp.className = ''
-
-        email_label.className = ''
-        password_label.className = ''
 
         if (email_inp.value === "" || email_inp == null){
-            email_inp.className = 'failed'
-            email_label.className = 'empty-field'
+            email_inp.classList.add('failed')
+            email_label.classList.add('empty-field')
             error = true
         }
         if (password_inp.value === "" || password_inp == null){
-            password_inp.className = 'failed'
-            password_label.className = 'empty-field'
+            password_inp.classList.add('failed')
+            password_label.classList.add('empty-field')
             error = true
         }
         if (error === true){
